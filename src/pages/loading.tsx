3043ab0a7190a8b27b84c783d8124ef7dac8d6bc
Quang Tran <quangtv2702@gmail.com>
2023-11-25 12:07:46 +0700

    Fix vercel 404 error

--- src/pages/loading.tsx
+++ src/pages/loading.tsx
@@ -2,6 +2,7 @@ export default function Loading() {
   return (
     <div className='flex h-screen w-screen items-center justify-center'>
       <span className='loading'></span>
+      <span>Vì trang web sử dụng server miễn phí nên tốc độ tải trang có thể hơi chậm, xin thông cảm!</span>
     </div>
   )
 }
